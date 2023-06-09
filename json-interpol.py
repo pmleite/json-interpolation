0a593313fe719097e0f884a931411c3ca192ad0f
pmleite <paulo.miguel.r.leite@gmail.com>
2023-06-09 13:44:52 +0000

    update

--- json-interpol.py
+++ json-interpol.py
@@ -8,30 +8,49 @@ base_output_file_name:str   = 'realm_'
 json_files:list             = []
 realms_dict:dict            = {}
 
-#Get a list of JSON files in a folder
-def getListOfJSONFileNames(folder:str = input_folder):
-    for filename in os.listdir(folder):
-        if filename.endswith(".json"):
-            json_files.append(filename)
 
 #Creates a dictionary of JSON files agrupaded by the word after the first dash of file name
-def createJSONFileDictionary(folder:str = input_folder):
-    json_files = getListOfJSONFileNames(folder)
-    for filename in json_files:
-        if filename.endswith(".json"):
-            json_file_name = filename.split('-')[1]
+def create_json_file_dictionary(folder:str = input_folder):
+    
+    #Update the list of json files
+    for f in os.listdir(folder):
+        if f.endswith(".json"):
+            json_files.append(f)
+    
+    #Update the dictionary of json files
+    for ff in json_files:
+        if ff.endswith(".json"):
+            json_file_name = ff.split('-')[1]
             if json_file_name not in realms_dict:
                 realms_dict[json_file_name] = []
-            realms_dict[json_file_name].append(filename)
-
-# Get realm name from JSON file
-def get_realm_name(json_data):
-    return json_data[0]['realm']
-
-
+            realms_dict[json_file_name].append(ff)
+            
+    #List of json files by key of realms_dict
+    for key in realms_dict:
+        print(key)
+        print(realms_dict[key])
+        print()
+        
+    #Loop through the dictionary of json files, interpolate them and save them in the output folder
+    for realm in realms_dict:
+        print(realm)
+        print(realms_dict[realm])
+        interpolated_json = {}
+        for json_file in realms_dict[realm]:
+            #print(json_file)
+            with open(input_folder + json_file) as json_file:
+                data = json.load(json_file)
+                interpolated_json = {**interpolated_json, **data}
+        print(interpolated_json)
+        with open(output_folder + base_output_file_name + realm + '.json', 'w') as outfile:
+            json.dump(interpolated_json, outfile)
+            
 def main():
-    print(createJSONFileDictionary())
-    print(realms_dict)
+    create_json_file_dictionary()
+
   
+
 if __name__ == "__main__":
     main()
+
+    
