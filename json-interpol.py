a051709aba4c1d366b74f7ab04dd3bc9bc15d8a1
pmleite <paulo.miguel.r.leite@gmail.com>
2023-06-16 15:37:40 +0000

    Adding verbose mode

--- json-interpol.py
+++ json-interpol.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 import os   
 import argparse
@@ -9,17 +10,20 @@ parser.description += "Example: 'users-abc-file.json', 'groups-abc-file.json', w
 parser.add_argument("-j","--jsonsDir",  help="The folder where the JSON files are located, default './jsons/'. This folder MUST exist", type=str, default="./jsons/")
 parser.add_argument("-o","--outDir", help="The folder where the interpolated JSON files will be saved, default './jsons/out/'. This folder MUST exist", type=str)
 parser.add_argument("-b","--baseName", help="The base name of the interpolated JSON files, default 'merged_'", type=str, default="merged_")
+parser.add_argument("-v","--verbose", help="Verbose mode", action="store_true")
 args = parser.parse_args()
 
 input_folder:str            = args.jsonsDir
 output_folder:str           = args.outDir
 base_output_file_name:str   = args.baseName
+verbose_mode:bool           = args.verbose
 
 json_files:list             = []
 realms_dict:dict            = {}
 
+
 #Creates a dictionary of JSON files agrupaded by the word after the first dash of file name
-def create_json_file_dictionary(inFolder:str = input_folder, outFolder:str = output_folder):
+def create_json_file_dictionary(inFolder:str = input_folder, outFolder:str = output_folder, verbose:bool = verbose_mode):
     """
     Update an JSON file dictionary with the files in the folder,
     agrupaded by the word after the first dash of file name
@@ -52,33 +56,33 @@ def create_json_file_dictionary(inFolder:str = input_folder, outFolder:str = out
     #Update the list of json files
     for f in os.listdir(inFolder):
         if f.endswith(".json"):
+            if (verbose):print("Found the file: " + f)
             json_files.append(f)
+    if (verbose):print("\n:The list of files is: " + str(json_files) + "\n")
     
     #Update the dictionary of json files
     for ff in json_files:
         if ff.endswith(".json"):
             json_file_name = ff.split('-')[1]
             if json_file_name not in realms_dict:
+                if (verbose):print("Creating the key: " + json_file_name)
                 realms_dict[json_file_name] = []
             realms_dict[json_file_name].append(ff)
-    
+            if (verbose):print("Adding the file: " + ff + " to the key: ") 
+            
     #Interpolate the json filespyi
     for key in realms_dict: 
         merged:dict      = {} 
-        print("Merging the files: " + str(realms_dict[key]))
         for json_file in realms_dict[key]:
-            
             data = json.load(open(inFolder + json_file))
-            print("Merging the file: " + json_file)
             merged.update(data)
+        if(verbose):print("The file " + json_file + " was merged into the file " + base_output_file_name + key + ".json")
                
-        #Save the interpolated json file       
+        #Save the interpolated json file     
         with open(outFolder + base_output_file_name + key + '.json', 'w') as outfile:
             json.dump(merged, outfile, indent=2)
-            print(merged)
-            print("The interpolated file: " + base_output_file_name + key + '.json was saved in the folder: ' + outFolder + "\n")
-  
-       
+            if(verbose):print("The file " + base_output_file_name + key + ".json was created into the folder " + outFolder )
+    
 def main():
     create_json_file_dictionary()
 
